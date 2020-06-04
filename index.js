728435b0cfe007f2f658b83fa091cf479dc80b36
naroc <choyonglee@naver.com>
2020-06-04 17:59:53 +0900

    8강 수정사항업데이트

--- index.js
+++ index.js
@@ -8,8 +8,39 @@ const mongodb = require('mongoose')
 mongodb.connect('mongodb+srv://admin1:naroc8978@node-react-test-zy0no.mongodb.net/<dbname>?retryWrites=true&w=majority',                  
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true
 }).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log('!!에러!!'+err))
 
+// body의 값을 받아올수 있게 선언
+const bodyParser = require('body-parser');
+
+// user 스키마 데이터 베이스 보델을 사용할수 있게 선언
+const { User } = require("./models/user");
+
+// bodyParser 보내지는 데이터를 받아서 분석할수 있게 하는 코딩
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}))
+
+// 분석해서 가져올때의 데이터 타입
+// application/json
+app.use(bodyParser.json());
+
+// 라우터1
 app.get('/', (req, res) => res.send('안녕하세요 NODE + REACT 입니다'))
+
+// 라우터2
+app.post('/register', (req, res) => {
+    /*
+        회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다
+    */
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+}) 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
