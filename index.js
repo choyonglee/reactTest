404d173af21ffb1480584300ed43606bff53606e
naroc <choyonglee@naver.com>
2020-06-15 10:58:49 +0900

    DB정보 보호

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-// mongodb 라이브러리 가져오기
-const mongodb = require('mongoose')
-mongodb.connect('mongodb+srv://admin1:naroc8978@node-react-test-zy0no.mongodb.net/<dbname>?retryWrites=true&w=majority',                  
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true
-}).then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log('!!에러!!'+err))
-
 // body의 값을 받아올수 있게 선언
 const bodyParser = require('body-parser');
 
+// 배포용과 개발용 config를 서로 다르게 읽어오게 바꿈
+const config = require('./config/key');
+
 // user 스키마 데이터 베이스 보델을 사용할수 있게 선언
-const { User } = require("./models/user");
+const { User } = require('./models/user');
 
 // bodyParser 보내지는 데이터를 받아서 분석할수 있게 하는 코딩
 // application/x-www-form-urlencoded
@@ -23,6 +19,13 @@ app.use(bodyParser.urlencoded({extended:true}))
 // 분석해서 가져올때의 데이터 타입
 // application/json
 app.use(bodyParser.json());
+
+// mongodb 라이브러리 가져오기
+const mongodb = require('mongoose')
+mongodb.connect(config.mongoURI,             
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log('!!에러!!'+err))
 
 // 라우터1
 app.get('/', (req, res) => res.send('안녕하세요 NODE + REACT 입니다'))
